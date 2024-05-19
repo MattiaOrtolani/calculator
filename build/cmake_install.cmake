cc7be430898bb11261d8e0cd90f23958eada1086
5kul1y <mattia.ortolani05@gmail.com>
2024-05-19 13:50:55 +0200

    modificato file CMake

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/mattia.ortolani/Programmi/C++/calculator/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/mattia.ortolani/GitHub/calculator/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
